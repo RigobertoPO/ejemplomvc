75b61b1f3294d349db841d0a810f4f2ae3f20080
rigobertopo <rperez@cobach.edu.mx>
2025-04-24 20:00:00 -0600

    se crea model gastos

---
+++ model/gastosmodel.php
@@ -0,0 +1,39 @@
+<?php
+class Gasto{
+    private $listaGastos;
+    public function __construct()
+    {
+        $this->listaGastos=array();
+    }
+    //CREATE
+    public function insertarGasto($descripcion,$monto,$idcategoria,$fecha){
+        include_once("conexion.php");
+        $cnn=new Conexion();
+        $consulta="INSERT INTO gastos(Descripcion,Monto,
+        IdCategoria,FechaRegistro,Activo)
+        VALUES($descripcion,$monto,$idcategoria,$fecha,1)";
+        $resultado=$cnn->prepare($consulta);
+        $resultado->execute();
+        if($resultado){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    //READ
+    public function obtenerGastos(){
+        include_once("conexion.php");
+        $cnn=new Conexion();
+        $consulta="SELECT * FROM gastos";
+        $resultado = $cnn->prepare($consulta);
+        $resultado->execute();
+        while ($row = $resultado->fetchAll(PDO::FETCH_ASSOC)) {
+            $this->listaGastos[]=$row;
+        }
+        return $this->listaGastos;    
+    }
+    //UPDATE
+    //DELETE
+}
+?>
--- model/indexmodel.php
+++ model/indexmodel.php
@@ -1,8 +1,10 @@
 <?php
 class Index{
     private $listaGastos;
+    private $usuarios;
     public function __construct() {
         $this->listaGastos = array();
+        $this->usuarios = array();
     }
     public function mostrarUltimosGastos(){
         include_once("conexion.php");
@@ -14,8 +16,19 @@ class Index{
         while ($row = $resultado->fetchAll(PDO::FETCH_ASSOC)) {
             $this->listaGastos[]=$row;
         }
-        return $this->listaGastos;
-        
+        return $this->listaGastos;    
+    }
+    public function autentificacion($correo,$password){
+        include_once("conexion.php");
+        $cnn=new Conexion();
+        $consulta="SELECT * FROM usuarios 
+        where Correo=".$correo." And Password=".$password;
+        $resultado = $cnn->prepare($consulta);
+        $resultado->execute();
+        while ($row = $resultado->fetchAll(PDO::FETCH_ASSOC)) {
+            $this->usuarios[]=$row;
+        }
+        return $this->usuarios;    
     }
 }
 ?>
